8c63e0aba4ff1bfdfd182ed3525ab4bf0f8d5cca
azzamsa <me@azzamsa.com>
2021-10-16 11:32:45 +0700

    style: format with black

--- tutorial/tutorial/spiders/quotes.py
+++ tutorial/tutorial/spiders/quotes.py
@@ -2,19 +2,19 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes" # must be unique
+    name = "quotes"  # must be unique
 
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        "http://quotes.toscrape.com/page/1/",
+        "http://quotes.toscrape.com/page/2/",
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
+        for quote in response.css("div.quote"):
             # `foo.bar` the dot operator to access the class
             # `::text`
             yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
             }
