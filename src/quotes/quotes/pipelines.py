72e434f773ec7d5e6cc1148a6d6f01c4a8f71611
azzamsa <me@azzamsa.com>
2021-10-23 15:05:08 +0700

    feat: store quotes to sqlite3 db

--- src/quotes/quotes/pipelines.py
+++ src/quotes/quotes/pipelines.py
@@ -3,11 +3,36 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
-class QoutesPipeline:
+class QuotesPipeline:
+    def __init__(self):
+        self.connect()
+        self.create_table()
+
+    def connect(self):
+        self.conn = sqlite3.connect("quotes.db")
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("DROP TABLE IF EXISTS quotes")
+        self.curr.execute("""CREATE TABLE quotes(text text,  author text, tag text)""")
+
     def process_item(self, item, spider):
+        self.store(item)
         return item
+
+    def store(self, item):
+        self.curr.execute(
+            """INSERT INTO quotes values (?,?,?)""",
+            (
+                item["text"],
+                item["author"],
+                item["tags"][0],
+            ),
+        )
+        self.conn.commit()
