aa08c3f8f539a84d38b7944053e59ceb6a6cc820
azzamsa <me@azzamsa.com>
2021-10-23 13:53:58 +0700

    refactor: use `Items` instead of Python dictionary
    Compared to Python dictionary, Scrapy item has a better structure.
    Thus avoiding typos, inconsistent data, and better messaging with Item loaders.
    
    Scrapy Items also called a temporary container. Because it is used as a model
    before saving to the db.

--- src/quotes/quotes/items.py
+++ src/quotes/quotes/items.py
@@ -6,7 +6,9 @@
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
+class QoutesItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
+
--- src/quotes/quotes/spiders/quotes.py
+++ src/quotes/quotes/spiders/quotes.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import QoutesItem
 
 
 class QuotesSpider(scrapy.Spider):
@@ -9,22 +10,28 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        items = QoutesItem()
+
         for quote in response.css("div.quote"):
             # `foo.bar` the dot operator to access the class
             # `::text` to extract the text only
-            yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                # `div.tags`, `tags` is a class
-                # `a.tag`, `tag` is a class
-                # a space denotes a children element
-                "tags": quote.css("div.tags a.tag::text").getall(),
-            }
+            text = quote.css("span.text::text").get()
+            author = quote.css("small.author::text").get()
+            # `div.tags`, `tags` is a class
+            # `a.tag`, `tag` is a class
+            # a space denotes a children element
+            tags = quote.css("div.tags a.tag::text").getall()
+
+            items["text"] = text
+            items["author"] = author
+            items["tags"] = tags
+
+            yield items
 
         next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
             # for learning purpose, 3 page is enough
             page_number = next_page.split("/")[2]
-            self.logger.info(f'Current page: {page_number}')
+            self.logger.info(f"Current page: {page_number}")
             if int(page_number) < 3:
                 yield response.follow(next_page, callback=self.parse)
