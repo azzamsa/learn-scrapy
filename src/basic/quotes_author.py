56d7deddd197cf2bf1dcfada1896a6f3ba86cb0e
azzamsa <me@azzamsa.com>
2021-10-26 06:59:22 +0700

    feat(basic): scrap author details
    BUG: only half of the quotes scraped. Probably due to duplicate link.
    
    ```
     'downloader/request_method_count/GET': 110,
     'downloader/response_count': 110,
     'downloader/response_status_count/200': 60,
     'downloader/response_status_count/308': 50,
     'dupefilter/filtered': 50,
    ```

---
+++ src/basic/quotes_author.py
@@ -0,0 +1,36 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes_author"
+    start_urls = ["http://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            author = quote.css("small.author::text").get()
+            text = quote.css("span.text::text").get()
+            tags = quote.css("div.tags a.tag::text").getall()
+
+            author_page = quote.css(".author + a ::attr(href)").get()
+            yield scrapy.Request(
+                f"https://quotes.toscrape.com{author_page}",
+                callback=self.parse_author,
+                cb_kwargs={"author": author, "text": text, "tags": tags},
+            )
+
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+    def parse_author(self, response, author, text, tags):
+        date = response.css(".author-born-date::text").get()
+        location = response.css(".author-born-location::text").get()
+        location = location.replace("in ", "")  # strip `in`
+
+        yield {
+            "author": author,
+            "date": date,
+            "location": location,
+            "text": text,
+            "tags": tags,
+        }
