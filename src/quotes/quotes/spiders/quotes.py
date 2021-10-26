73d4da90a7e71ae2c802f3a9ed2c7e54a1f859da
azzamsa <me@azzamsa.com>
2021-10-26 07:39:55 +0700

    feat(quotes): scrap author details
    Duplicate quotes fixed by creating `item` object inside `parse_author()`
    instead of `parse()`.
    
    Only half of the total quotes are crawled fixed by turning on the `dont_filter`.

--- src/quotes/quotes/spiders/quotes.py
+++ src/quotes/quotes/spiders/quotes.py
@@ -11,38 +11,35 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        item = QoutesItem()
-
         for quote in response.css("div.quote"):
-            text = quote.css("span.text::text").get()
             author = quote.css("small.author::text").get()
+            text = quote.css("span.text::text").get()
             tags = quote.css("div.tags a.tag::text").getall()
 
-            item["text"] = text
-            item["author"] = author
-            item["tags"] = tags
-
             author_page = quote.css(".author + a ::attr(href)").get()
-            yield scrapy.Request(
-                f"https://quotes.toscrape.com{author_page}",
+            yield response.follow(
+                author_page,
                 callback=self.parse_author,
-                cb_kwargs={"item": item},
+                dont_filter=True,
+                cb_kwargs={"author": author, "text": text, "tags": tags},
             )
 
         next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
-            # for learning purpose, 3 page is enough
-            page_number = next_page.split("/")[2]
-            self.logger.info(f"Current page: {page_number}")
-            if int(page_number) < 3:
-                yield response.follow(next_page, callback=self.parse)
+            self.logger.info(f"Next page: {next_page}")
+            yield response.follow(next_page, callback=self.parse)
 
-    def parse_author(self, response, item):
+    def parse_author(self, response, author, text, tags):
+        item = QoutesItem()
+
         date = response.css(".author-born-date::text").get()
         location = response.css(".author-born-location::text").get()
         location = location.replace("in ", "")  # strip `in`
 
+        item["author"] = author
         item["date"] = date
         item["location"] = location
+        item["text"] = text
+        item["tags"] = tags
 
         return item
