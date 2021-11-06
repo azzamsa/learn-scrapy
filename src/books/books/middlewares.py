d034fd40e773c16c76e6094e2d81e1c01b3fdd37
azzamsa <me@azzamsa.com>
2021-11-06 14:09:55 +0700

    refactor: blacken

--- src/books/books/middlewares.py
+++ src/books/books/middlewares.py
@@ -53,7 +53,7 @@ class BooksSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class BooksDownloaderMiddleware:
@@ -100,4 +100,4 @@ class BooksDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
