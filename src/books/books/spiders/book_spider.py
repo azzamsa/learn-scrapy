61c229fb040a1b676169dad3d0c901bc428e5e64
azzamsa <me@azzamsa.com>
2021-11-06 13:16:59 +0700

    feat(books): parse the whole site

--- src/books/books/spiders/book_spider.py
+++ src/books/books/spiders/book_spider.py
@@ -12,6 +12,10 @@ class BookSpiderSpider(scrapy.Spider):
             book_url = book.css("h3 a::attr(href)").get()
             yield response.follow(book_url, callback=self.parse_book)
 
+        next_page = response.css(".next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
     def parse_book(self, response):
         title = response.css("h1::text").get()
         price = response.css(".price_color::text").get()
